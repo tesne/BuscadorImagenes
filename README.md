0f01114fd466b481b04618ef6c3091d9f7177409
Francisco Perez <texnext@gmail.com>
2019-10-31 10:20:48 -0400

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# BuscadorImagenes
-Created with CodeSandbox
---
+++ src/components/App.js
@@ -0,0 +1,96 @@
+import React, { Component } from "react";
+import Buscador from "./Buscador";
+import Resultado from "./Resultado";
+
+class App extends Component {
+  state = {
+    termino: " ",
+    imagenes: [],
+    pagina: ""
+  };
+  scroll = () => {
+    const elemento = document.querySelector(".jumbotron");
+    elemento.scrollIntoView("smooth", "start");
+  };
+
+  consultarApi = () => {
+    //const busqueda = this.state.termino;
+    const pagina = this.state.pagina;
+    const url = `https://pixabay.com/api/?key=13625848-e0c7b2145b2d5ed589f06814f&q=${
+      this.state.termino
+    }&page=${pagina}`;
+    //console.log(url);
+    fetch(url)
+      .then(repuesta => repuesta.json())
+      .then(resultado => this.setState({ imagenes: resultado.hits }));
+  };
+
+  paginaAnterior = () => {
+    //leer state de la página actual
+    let pagina = this.state.pagina;
+    //leer si la página es 1 no ir hacia atras
+    if (pagina === 1) return null;
+    //restar 1 a la página actual
+    pagina -= 1;
+    //agregar cambio al state
+    this.setState(
+      {
+        pagina
+      },
+      () => {
+        this.consultarApi();
+        this.scroll();
+      }
+    );
+    // console.log(pagina);
+  };
+  paginaSiguiente = () => {
+    //leer state de la página actual
+    let pagina = this.state.pagina;
+    //sumar 1 a la página actual
+    pagina += 1;
+    //agregar cambio al state
+    this.setState(
+      {
+        pagina
+      },
+      () => {
+        this.consultarApi();
+        this.scroll();
+      }
+    );
+    //console.log(pagina);
+  };
+
+  datosBusqueda = termino => {
+    this.setState(
+      {
+        termino: termino,
+        pagina: 1
+      },
+      () => {
+        this.consultarApi();
+        this.scroll();
+      }
+    );
+  };
+
+  render() {
+    return (
+      <div className="App container">
+        <div className="jumbotron">
+          <p className="lead text-center">
+            <b>Buscador de imágenes</b>
+          </p>
+          <Buscador datosBusqueda={this.datosBusqueda} />
+        </div>
+        <Resultado
+          imagenes={this.state.imagenes}
+          paginaAnterior={this.paginaAnterior}
+          paginaSiguiente={this.paginaSiguiente}
+        />
+      </div>
+    );
+  }
+}
+export default App;
